7dc663c5ffa6e21d712f808b10f783d8cff6ad47
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-19 17:03:37 -0400

    add variables lesson

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-// learning console
-// console.log("one statement");
-// console.log("one statement");
-// console.log("one statement");
-/*console.log("one statement");
-console.log("one statement");
-console.log("one statement");
-*/
-document.write("hi there");
+//variables
+
+let name1 = "sunny";
+let adress, zipcode, state;
+adress = "101";
+zipcode = "30720";
+state = "GA";
+adress = "as";
+
+// console.log(adress);
+// console.log(state);
+// console.log(adress, state, zipcode);
+
+let Random123_$ = "random";
+
+console.log(Random123_$);
+
+// var
+var value = "some value";
+// let
+let name2 = "some name";
+name2 = "some name2";
+// const
+const lastName = "jordan";
+lastName = "jordan23";
+
+console.log(value);
+console.log(name2);
+console.log(lastName);
