421d481c44d648a536575e16595714839d46e587
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-20 17:34:05 -0400

    add array challenge

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// arrays
+// arrays challenge
 
-const friend1 = 'ana';
-const friend2 = 'ana';
-const friend3 = 'ana';
-const friend4 = 'ana';
+const fruits = ['banana', 'watermelon', 'mango', 10];
 
-const myArrayFriends = [friend1, friend2, friend3, friend4, 10, 'joe'];
+const firstFruit = fruits[0];
+fruits[3] = 'banana';
 
-console.log(myArrayFriends[7]);
-
-myArrayFriends[4] = 'tim';
-
-console.log(myArrayFriends);
+console.log(firstFruit, fruits);
