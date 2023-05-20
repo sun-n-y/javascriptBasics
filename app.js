6e1bab13bf775e3b0b045365f286575d911c6f7b
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-20 13:55:05 -0400

    add numbers basics

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-// string concatenation challenge
+// numbers
 
-const street = 'manley street';
-const country = 'usa';
-let fullMailingAddress = street + ' ' + country;
+// const number = 1;
+// const number2 = 4.5;
+// const number3 = 4;
 
-console.log(fullMailingAddress);
+// const add = number + number2;
+// const subtract = number - number2;
+// const multiply = number * number2;
+// const divide = number / number2;
+
+// console.log(add);
+// console.log(subtract);
+// console.log(multiply);
+// console.log(divide);
+
+let number = 40;
+
+number++;
+number++;
+number++;
+number += 1;
+
+const slices = 10;
+const kids = 7;
+const amount = slices % kids;
+
+console.log(amount);
