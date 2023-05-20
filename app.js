1e7e9fb6ebe02158be10f2b43a156c34203e4cfa
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-20 14:18:54 -0400

    add numbers basic challenge

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-// numbers
+// numbers challenge
 
-// const number = 1;
-// const number2 = 4.5;
-// const number3 = 4;
+const score1 = 98;
+const score2 = 75;
+const score3 = 45;
+let amount = 3;
 
-// const add = number + number2;
-// const subtract = number - number2;
-// const multiply = number * number2;
-// const divide = number / number2;
+let totalScore = score1 + score2 + score3;
+let average = totalScore / amount;
 
-// console.log(add);
-// console.log(subtract);
-// console.log(multiply);
-// console.log(divide);
+console.log(totalScore, average);
 
-let number = 40;
+const plates = 20;
+const people = 7;
 
-number++;
-number++;
-number++;
-number += 1;
+let remainingPlates = 20 % 7;
 
-const slices = 10;
-const kids = 7;
-const amount = slices % kids;
+remainingPlates++;
 
-console.log(amount);
+let msg = 'There are' + ' ' + remainingPlates + ' ' + 'plates avaibale';
+
+console.log(msg);
