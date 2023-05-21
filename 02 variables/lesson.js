14ddb7772a6ef1774b34276ba65133cb98da8e20
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-21 11:19:12 -0400

    add variables lesson

---
+++ 02 variables/lesson.js
@@ -0,0 +1,45 @@
+//variables
+
+// let name1 = 'spiderman 5';
+
+// console.log(name1);
+// //some code
+
+// console.log(name1);
+// //some code
+
+// console.log(name1);
+// //some code
+
+// console.log(name1);
+// //some code
+
+// console.log(name1);
+// //some code
+
+// let name1, lastName;
+
+// console.log(name1, lastName);
+
+// name1 = 'sunny';
+
+// console.log(name1, lastName);
+
+// name1 = 'sunny007';
+
+// console.log(name1, lastName);
+
+// var
+var value = 'some values';
+
+//let
+let name2 = 'joe';
+
+//const
+const lastName = 'polo';
+
+console.log(value);
+console.log(name2);
+
+// lastName = 'ralph';
+console.log(lastName);
