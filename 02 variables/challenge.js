d737681808f31e586f20ed6198c66907df35e87d
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-21 11:26:26 -0400

    add variables challenge

---
+++ 02 variables/challenge.js
@@ -0,0 +1,10 @@
+const firstName = 'john';
+var last_name = 'doe';
+
+let address = 'main street';
+
+console.log(firstName, last_name, address);
+
+address = 'first street';
+
+console.log(firstName, last_name, address);
