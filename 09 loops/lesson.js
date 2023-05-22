92d5fc44cde1dada4934f3035b056c57772e3a54
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-22 12:10:11 -0400

    add loops lesson

---
+++ 09 loops/lesson.js
@@ -0,0 +1,29 @@
+//loops
+
+// //while loop----------------------
+// let amount = 10;
+
+// while (amount > 0) {
+//   console.log('i have ' + amount + " dollars and i'm going to the mall");
+//   amount--;
+// }
+
+// // do while loop-------------------
+// let money = 12;
+
+// do {
+//   console.log('you have ' + money + ' dollars');
+//   money++;
+// } while (money < 10);
+
+//for loop-------------------------
+
+// let i;
+
+// for (i = 0; i < 10; i++) {
+//   console.log('and the number is: ' + i);
+// }
+
+for (let number = 11; number >= 0; number--) {
+  console.log('and the number is ' + number);
+}
