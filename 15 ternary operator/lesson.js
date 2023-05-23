9a13ce9b2b3ba656cb0a660233adb30bc43497c6
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-23 13:17:20 -0400

    add ternary operators lesson

---
+++ 15 ternary operator/lesson.js
@@ -0,0 +1,20 @@
+//unary operator - type of
+let text = 'some text';
+// console.log(typeof text);
+//binary operator - assignment
+let number = 3;
+let number2 = 2 + 5;
+//ternary operator
+//condition ? (runs if true) : (runs if false)
+
+const value = 2 < 1;
+
+// if (value) {
+//   console.log('value is true');
+// } else {
+//   console.log('value is false');
+// }
+
+//ternary example
+
+value ? console.log('ture') : console.log('false');
