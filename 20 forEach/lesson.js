acb90870c01176ba395fd770dc9acf5ee4dd229a
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-23 20:23:25 -0400

    add for each lesson

---
+++ 20 forEach/lesson.js
@@ -0,0 +1,17 @@
+//forEach
+
+const people = [
+  { name: 'bob', age: 20, position: 'developer' },
+  { name: 'peter', age: 25, position: 'designer' },
+  { name: 'susy', age: 30, position: 'the boss' },
+];
+
+function showPerson(person) {
+  console.log(person.position.toUpperCase());
+}
+
+// people.forEach(showPerson);
+
+people.forEach(function (item) {
+  console.log(item.position.toUpperCase());
+});
