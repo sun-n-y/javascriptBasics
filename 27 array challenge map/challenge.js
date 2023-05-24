ae44f00535d9d1843fccb3c392ee99e064c9c7c1
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-23 23:23:39 -0400

    add array map challenge

--- 27 array challenge map/challenge.js
+++ 27 array challenge map/challenge.js
@@ -1,1 +1,9 @@
 //array challenge
+
+const updatedStudents = students.map(function (student) {
+  //   console.log(student);
+  student.role = 'student';
+  return student;
+});
+
+console.log(updatedStudents);
