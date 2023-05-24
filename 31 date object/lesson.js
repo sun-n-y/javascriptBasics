050652a22f8fcb676c50b1f0e6e07bf274aaecc6
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-24 16:24:24 -0400

    add date object lesson

---
+++ 31 date object/lesson.js
@@ -0,0 +1,46 @@
+//date object
+
+const months = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thrusday',
+  'Friday',
+  'Saturday',
+];
+
+// const date = new Date();
+const date = new Date('1/12/2004');
+// console.log(date);
+
+//need to refernce it to get actual day instead of index
+const month = date.getMonth();
+console.log(months[month]);
+
+const day = date.getDay();
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const sentence = `${days[day]}, ${date.getDate()} ${
+  months[month]
+} ${date.getFullYear()}`;
+
+document.body.innerHTML = sentence;
