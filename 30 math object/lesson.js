335bd8d4f6bbd3d90c84e249b5c1db426863295e
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-24 15:52:36 -0400

    add math object lesson

---
+++ 30 math object/lesson.js
@@ -0,0 +1,17 @@
+//math object
+
+// const number = 4.56789;
+// const result = Math.floor(8.999);
+
+// const number = 4.56789;
+// const result = Math.ceil(number);
+
+// const number = 25;
+// const result = Math.sqrt(number);
+
+// const result = Math.PI;
+// const result = Math.min(4, 5, 6, 78);
+// const result = Math.max(4, 5, 6, 78);
+const result = Math.floor(Math.random() * 10 + 1);
+
+console.log(result);
