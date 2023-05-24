e8cb082dbf621de7a81fb5d2cbef5763b9714cb1
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-24 15:36:24 -0400

    add array reduce 2 challenge

--- 28 array challenges reduce/challenge.js
+++ 28 array challenges reduce/challenge.js
@@ -1,9 +1,24 @@
-//array challenge reduce
+// //array challenge reduce
 
-const averageScore =
-  students.reduce(function (acc, curr) {
-    acc += curr.score;
-    return acc;
-  }, 0) / students.length;
+// const averageScore =
+//   students.reduce(function (acc, curr) {
+//     acc += curr.score;
+//     return acc;
+//   }, 0) / students.length;
 
-console.log(averageScore);
+// console.log(averageScore);
+
+//square bracket notation
+
+const survey = students.reduce(function (survey, student) {
+  // console.log(student.favoriteSubject);
+  const favSubject = student.favoriteSubject;
+  if (survey[favSubject]) {
+    survey[favSubject] = survey[favSubject] + 1;
+  } else {
+    survey[favSubject] = 1;
+  }
+  return survey;
+}, {});
+
+console.log(survey);
