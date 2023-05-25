8c501ad6264f00a9ccf8940e04f9235138bb839f
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-25 12:25:50 -0400

    add array challenge 2

---
+++ 32 array challenges 2/challenge.js
@@ -0,0 +1,79 @@
+//array challenge
+
+//forEach
+// const updatedStudents2 = students.forEach(function (student) {
+//   console.log(student.name.toUpperCase());
+// });
+
+//MAP
+// const updatedStudents = students.map(function (student) {
+//   //   console.log(student);
+
+//   student.role = 'student';
+
+//   return student;
+// });
+
+// console.log(updatedStudents);
+
+//MAP 2
+
+// const updatedStudents3 = students.map(function (student) {
+//   return student.score;
+// });
+
+// const newPeople = students.map(function (student) {
+//   return {
+//     FIRSTNAME: student.name.toUpperCase(),
+//   };
+// });
+
+// console.log(newPeople);
+
+// filter
+// const highScores = students.filter(function (student) {
+//   if (student.score >= 80) return student;
+// });
+
+// console.log(highScores);
+
+//find
+// const specificId = students.find(function (student) {
+//   return student.id === 3;
+// });
+
+// console.log(specificId);
+
+//reduce
+
+// const averageScore =
+//   students.reduce(function (acc, curr) {
+//     acc += curr.score;
+//     return acc;
+//   }, 0) / students.length;
+
+// console.log(averageScore);
+
+//reduce 2
+//square bracket notation
+
+const subject = 'math';
+
+//creating a new object with name of total
+const total = {};
+
+total[subject] = 'some value';
+// console.log(total);
+
+const survey = students.reduce(function (survey, student) {
+  //   console.log(student.favoriteSubject);
+  const favSubject = student.favoriteSubject;
+  if (survey[favSubject]) {
+    survey[favSubject]++;
+  } else {
+    survey[favSubject] = 1;
+  }
+  return survey;
+}, {});
+
+console.log(survey);
